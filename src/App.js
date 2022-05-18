0caf9d527c4e1012e2d6700d010325a81f3d3327
ashish8449 <ashishgururani8449@gmail.com>
2022-05-18 22:23:07 +0530

    18may

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import "swiper/swiper.min.css";
+import "./assets/boxicons-2.0.7/css/boxicons.min.css";
 import "./App.scss";
+
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+
+import Routes from "./config/Routes";
+
 function App() {
-  return <>jhey this is my app componet</>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes />
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/api/apiconfig.js
+++ src/api/apiconfig.js
@@ -1,4 +1,8 @@
 const apiConfig = {
-  baseUrl: "https://api.themovied.org/3/",
-  apiKey:''
+  baseUrl: "https://api.themoviedb.org/3/",
+  apiKey: "70c2c6887c3aabc39030e394366f70a3",
+  originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
+  w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 };
+
+export default apiConfig;
