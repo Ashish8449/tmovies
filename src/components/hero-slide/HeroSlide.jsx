c0cd3e1e4a936a6979b1d76eabe61cb475c828be
ashish8449 <ashishgururani8449@gmail.com>
2022-05-21 21:12:45 +0530

    change

--- src/components/hero-slide/HeroSlide.jsx
+++ src/components/hero-slide/HeroSlide.jsx
@@ -39,7 +39,7 @@ const HeroSlide = () => {
                 grabCursor={true}
                 spaceBetween={0}
                 slidesPerView={1}
-                // autoplay={{delay: 3000}}
+                autoplay={{delay: 3000}}
             >
                 {
                     movieItems.map((item, i) => (
--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 
 import tmdbApi from '../../api/tmdbApi';
-// import apiConfig from '../../api/apiConfig';
+
 
 
 import './detail.scss';
