cb945dd3c385868734f5d7a50ee2a616200d54a4
Anmol Gangwar <anmolgangwar64@gmail.com>
2023-02-06 23:40:13 +0530

    --

--- src/api/apiconfig.js
+++ src/api/apiconfig.js
@@ -1,6 +1,6 @@
 const ApiConfig = {
   baseUrl: `https://api.themoviedb.org/3/`,
-  apiKey: process.env.REACT_API_KEY,
+  apiKey: "70c2c6887c3aabc39030e394366f70a3",
   originalImage: (imagePath) =>
     `https://image.tmdb.org/t/p/original/${imagePath}`,
   W500Image: (imagePath) => `https://image.tmdb.org/t/p/w500/${imagePath}`,
